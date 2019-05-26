aca5fbf87098f3d029cc49cabbb32ee50a0cbfc0
Richard Le <richardle1997@gmail.com>
2019-05-25 20:32:35 -0700

    First commit, working test server and background img

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname));
+app.use(express.static(__dirname + 'css'));
+
+const port = 8080;
+
+app.get('/', function (req, res){
+	res.sendfile(__dirname + 'index.html');
+});
+
+app.listen(port, function(){
+	console.log("Listening on port " + port);
+});
