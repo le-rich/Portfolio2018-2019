51f262ec2622752bbb6eabeda81cc0462e11b34b
Richard Le <richardle1997@gmail.com>
2019-05-26 20:42:48 -0700

    First landing prototype done

--- js/landing.js
+++ js/landing.js
@@ -1,5 +1,14 @@
 let b = baffle('#rl-logo', {
 	characters: '>/</ ▓█░|'
 }).start();
+b.reveal(1500,0);
+
+let b2 = baffle("#prof-label", {
+	characters: '>/</ ▓█░|'
+}).start();
+b2.reveal(3000,0);
 
 
+$("#resume-nav").click(function(){
+	$("#background").css({"transform":"translateY(-50%)"});
+});
