e17777e70d7f48eedf54163c6d57355324406e88
Richard Le <richardle1997@gmail.com>
2019-05-27 14:43:12 -0700

    Sexy as hell swipe down to resume implemented

--- js/landing.js
+++ js/landing.js
@@ -8,7 +8,25 @@ let b2 = baffle("#prof-label", {
 }).start();
 b2.reveal(3000,0);
 
-
 $("#resume-nav").click(function(){
-	$("#background").css({"transform":"translateY(-50%)"});
+	showResume();
 });
+
+$("#up-arrow").click(function(){
+	hideResume();
+});
+
+
+function showResume(){
+	$("#background").css({"transform":"translateY(-50%)"});
+	$("#ui-overlay-container").css({"transform": "translateY(-100%)", "opacity": "0"});
+	$("#resume-container").css({"top":"0%", "opacity": "1"});
+}
+
+function hideResume(){
+	$("#background").css({"transform":"translateY(0%)"});
+	$("#ui-overlay-container").css({"transform": "translateY(0)", "opacity": "1"});
+	$("#resume-container").css({"top":"300%", "opacity": "0"});
+}
+
+
