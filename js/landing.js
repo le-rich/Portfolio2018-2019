94a5fd8800a916149b0669abaf3b1eeddfbabfa0
Richard Le <richardle1997@gmail.com>
2019-05-27 17:32:43 -0700

    Beginning work on projects page, small tweaks to UI of navigation

--- js/landing.js
+++ js/landing.js
@@ -21,12 +21,14 @@ function showResume(){
 	$("#background").css({"transform":"translateY(-50%)"});
 	$("#ui-overlay-container").css({"transform": "translateY(-100%)", "opacity": "0"});
 	$("#resume-container").css({"top":"0%", "opacity": "1"});
+	$("#up-arrow").css({"cursor": "pointer"});
 }
 
 function hideResume(){
 	$("#background").css({"transform":"translateY(0%)"});
 	$("#ui-overlay-container").css({"transform": "translateY(0)", "opacity": "1"});
-	$("#resume-container").css({"top":"300%", "opacity": "0"});
+	$("#resume-container").css({"top":"0%", "opacity": "0"});
+	$("#up-arrow").css({"cursor": "none"});
 }
 
 
